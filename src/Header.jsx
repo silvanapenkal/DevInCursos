4d48ced8567c1e62502efd68f1f8305c91e403b9
silvanapenkal <silvanapenkal@gmail.com>
2023-03-07 22:27:31 -0300

    implementação do header

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export const Header = () => {
+    return <header>
+        <div className="headerDiv">
+            <img src="/src/assets/logo.svg"></img>
+        <h1>DevInCursos</h1>
+        </div>
+            </header>
+        ;
+  };
+
+  export default Header;
