aa0c1762dead4adf65260f2ee76d7a39b33ec8c5
silvanapenkal <silvanapenkal@gmail.com>
2023-03-07 23:00:33 -0300

    read.me

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DevInCursos
 Projeto das aulas de React do DevinHouse
+
+#Tecnologias
+
+React
+Javascript
+Projeto criado com Vite
+Prop-Types para a validação dos tipos
+
+Figma do projeto: https://www.figma.com/file/EgSy0PHZpnTDcvF5axe3Ee/DevInilog?node-id=0-1&t=ElayBUE9Vlnmavn7-0
---
+++ src/Card.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import "./Card.css";
+
+import clockImg from "../assets/clock.png";
+
+function CourseCard({ imageUrl, name, category, description, duration }) {
+  return (
+    <div className="cardContainer">
+      <div className="cardHeader">
+        {imageUrl && <img src={imageUrl} alt={`Imagem curso ${name}`} />}
+        <h3>{name}</h3>
+      </div>
+      <div className="courseCardDuration">
+        <img src={clockImg} alt="Ícone de relógio" />
+        <p>{duration}h</p>
+        <p>{category}h</p>
+        <p>{description}h</p>
+      </div>
+      <button className="courseCardActionButton">Ver detalhes</button>
+    </div>
+  );
+}
+
+CourseCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  duration: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string,
+};
+
+export default CourseCard;
