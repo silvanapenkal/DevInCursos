8f402e3892bcfee468577144624016a6a553b5ae
silvanapenkal <silvanapenkal@gmail.com>
2023-03-07 21:16:16 -0300

    instalação do prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <h1>DevInCursos</h1>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   )
 }
