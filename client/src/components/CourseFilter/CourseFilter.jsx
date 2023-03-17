09715a6dedef6fdcd9ca9b825f2b373d5c9b1ac1
silvanapenkal <silvanapenkal@gmail.com>
2023-03-17 19:30:56 -0300

    instalação do axios, configuração do server

--- client/src/components/CourseFilter/CourseFilter.jsx
+++ client/src/components/CourseFilter/CourseFilter.jsx
@@ -1,12 +1,42 @@
-import "./CourseFilter.css";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function CourseFilter() {
+import { BUTTON_VARIANT } from '../Button/ButtonVariant.js';
+import Button from '../Button/Button';
+import InputGroup from '../InputGroup/InputGroup';
+
+import './CourseFilter.css';
+
+function CourseFilter({ onFilter }) {
+  const [filter, setFilter] = useState('');
+
+  const handleChangeFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleClickFilter = () => {
+    onFilter(filter);
+  };
+
   return (
-    <div className="courseFilterContainer">
-      <input type="text" placeholder="Busque pelo nome do curso" />
-      <button>Buscar</button>
+    <div className='courseFilterContainer'>
+      <InputGroup
+        type='text'
+        placeholder='Busque pelo nome do curso'
+        labelText='Buscar'
+        value={filter}
+        onChange={handleChangeFilter}
+      />
+
+      <Button variant={BUTTON_VARIANT.PRIMARY_OUTLINED} onClick={handleClickFilter}>
+        Buscar
+      </Button>
     </div>
   );
 }
+
+CourseFilter.propTypes = {
+  onFilter: PropTypes.func,
+};
 
 export default CourseFilter;
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,88 +1,120 @@
-import CourseFilter from "../../components/CourseFilter/CourseFilter";
-import CourseList from "../../components/CourseList/CourseList";
-import "./HomePage.css";
+// import CourseFilter from "../../components/CourseFilter/CourseFilter";
+// import CourseList from "../../components/CourseList/CourseList";
+// import "./HomePage.css";
 
-import reactJsImg from "../../assets/reactjs.png";
-import reactTsImg from "../../assets/reactts.png";
-import reactRouterImg from "../../assets/reactrouter.png";
-import contextApiImg from "../../assets/contextapi.png";
-import { useDevinCourseContext } from "../../DevinCourseContext";
+// import reactJsImg from "../../assets/reactjs.png";
+// import reactTsImg from "../../assets/reactts.png";
+// import reactRouterImg from "../../assets/reactrouter.png";
+// import contextApiImg from "../../assets/contextapi.png";
+// import { useDevinCourseContext } from "../../DevinCourseContext";
 
-const listaDeCursos = [
-  {
-    id: 635,
-    imageUrl: reactJsImg,
-    name: "React Js",
-    category: "front-end",
-    description: "Curso sobre React Js",
-    duration: 6,
-  },
-  {
-    id: 873,
-    imageUrl: reactTsImg,
-    name: "React com TS",
-    category: "front-end",
-    description: "Curso sobre React com TS",
-    duration: 6,
-  },
-  {
-    id: 935,
-    imageUrl: reactRouterImg,
-    name: "React Router",
-    category: "front-end",
-    description: "Curso sobre React Router",
-    duration: 6,
-  },
-  {
-    id: 333,
-    imageUrl: contextApiImg,
-    name: "Context API",
-    category: "front-end",
-    description: "Curso sobre Context API",
-    duration: 6,
-  },
-  {
-    id: 844,
-    imageUrl: reactJsImg,
-    name: "React Js",
-    category: "front-end",
-    description: "Curso sobre React Js",
-    duration: 6,
-  },
-  {
-    id: 287,
-    imageUrl: reactTsImg,
-    name: "React com TS",
-    category: "front-end",
-    description: "Curso sobre React com TS",
-    duration: 6,
-  },
-  {
-    id: 118,
-    imageUrl: reactRouterImg,
-    name: "React Router",
-    category: "front-end",
-    description: "Curso sobre React Router",
-    duration: 6,
-  },
-  {
-    id: 589,
-    imageUrl: contextApiImg,
-    name: "Context API",
-    category: "front-end",
-    description: "Curso sobre Context API",
-    duration: 6,
-  },
-];
+// const listaDeCursos = [
+//   {
+//     id: 635,
+//     imageUrl: reactJsImg,
+//     name: "React Js",
+//     category: "front-end",
+//     description: "Curso sobre React Js",
+//     duration: 6,
+//   },
+//   {
+//     id: 873,
+//     imageUrl: reactTsImg,
+//     name: "React com TS",
+//     category: "front-end",
+//     description: "Curso sobre React com TS",
+//     duration: 6,
+//   },
+//   {
+//     id: 935,
+//     imageUrl: reactRouterImg,
+//     name: "React Router",
+//     category: "front-end",
+//     description: "Curso sobre React Router",
+//     duration: 6,
+//   },
+//   {
+//     id: 333,
+//     imageUrl: contextApiImg,
+//     name: "Context API",
+//     category: "front-end",
+//     description: "Curso sobre Context API",
+//     duration: 6,
+//   },
+//   {
+//     id: 844,
+//     imageUrl: reactJsImg,
+//     name: "React Js",
+//     category: "front-end",
+//     description: "Curso sobre React Js",
+//     duration: 6,
+//   },
+//   {
+//     id: 287,
+//     imageUrl: reactTsImg,
+//     name: "React com TS",
+//     category: "front-end",
+//     description: "Curso sobre React com TS",
+//     duration: 6,
+//   },
+//   {
+//     id: 118,
+//     imageUrl: reactRouterImg,
+//     name: "React Router",
+//     category: "front-end",
+//     description: "Curso sobre React Router",
+//     duration: 6,
+//   },
+//   {
+//     id: 589,
+//     imageUrl: contextApiImg,
+//     name: "Context API",
+//     category: "front-end",
+//     description: "Curso sobre Context API",
+//     duration: 6,
+//   },
+// ];
+
+// function HomePage() {
+//   const userInfo = useDevinCourseContext();
+//   const isAdmin = (userInfo[2].isAdmin);
+//   return (
+//     <div className="homePageContainer">
+//       {isAdmin && <button>Cadastrar Curso</button>}
+//       <CourseFilter />
+//       <CourseList list={listaDeCursos} />
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+
+// import { Spinner } from 'phosphor-react';
+
+import CourseFilter from '../../components/CourseFilter/CourseFilter';
+import CourseList from '../../components/CourseList/CourseList';
+import useCourseList from '../../hooks/useCourseList';
+
+import emptyState from '../../assets/empty.svg';
+
+import './HomePage.css';
 
 function HomePage() {
-  const userInfo = useDevinCourseContext();
-  const isAdmin = (userInfo[2].isAdmin);
+  const { courses, error, isLoading, fetchData } = useCourseList();
+
   return (
-    <div className="homePageContainer">
-      {isAdmin && <button>Cadastrar Curso</button>}
-      <CourseFilter />
-      <CourseList list={listaDeCursos} />
+    <div className='homePageContainer'>
+      <CourseFilter onFilter={fetchData} />
+
+      {/* {isLoading && <Spinner width={100} />} */}
+
+      {!isLoading && !!error && <p>{error}</p>}
+
+      {!isLoading && !error && !!courses.length && <CourseList list={courses} />}
+
+      {!isLoading && !error && !courses.length && (
+        <img height={500} src={emptyState} alt='Imagem de nenhum item encontrado' />
+      )}
     </div>
   );
 }
